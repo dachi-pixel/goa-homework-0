791bf49acbe263ba7c1af4d49557cef144a1c7a3
dachi-pixel <dachi.bolqvadze11@gmail.com>
2024-08-08 18:47:51 +0400

    sixth commit

--- level 6/homework/hw.py
+++ level 6/homework/hw.py
@@ -12,3 +12,9 @@ print(usersNumber - 5)
 user_name = input("enter your name: ")
 lastName = input("enter your last name: ")
 print("your", "name", "is", user_name, lastName)
+
+# 3) 
+userNum = int(input("enter your num: "))
+UserNum = input("enter your num: ")
+print(userNum * 5) # აქ პრობლემა არ არის რადგან აქ მიტიტებული მაქვს რომ ეს არის ინტჯერი
+print(UserNum * 5) # აქ ერორია და ამას დიბაგინგი სჭირდება რადგან სტრინგს ინტეჯერი არ ემატება
