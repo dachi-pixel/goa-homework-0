82fdad6835e17b7980597d1e40fdea506a665dc9
dachi-pixel <dachi.bolqvadze11@gmail.com>
2024-09-08 17:34:51 +0400

    twelfth commit

---
+++ level 12/homework/hw.py
@@ -0,0 +1,11 @@
+name = "dachi"
+password = "dachidachi123321@@##"
+user_name = input("enter your name:")
+user_password = input("enter your password:")
+
+if name == user_name and password == user_password:
+    print("you logged in your python aqq")
+else:
+    print("try again")
+    user_name = input("enter your name:")
+user_password = input("enter your password:")
